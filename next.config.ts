fba766005ba32b6a975bb19c628ebb15fff91922
Alberto Duarte <betodute@gmail.com>
2025-10-12 15:36:46 -0700

    configuration issues with npm vs pnpm

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",           
-  images: { unoptimized: true }
+  output: "export",           // tells Next to generate static HTML files in /out
+  images: { unoptimized: true } // required for static export when using <Image />
 };
 
 export default nextConfig;
